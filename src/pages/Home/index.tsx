ed2329650e0ad0552090a9f4586f3223fe1c1c5c
camille <mille.ouriques@gmail.com>
2024-02-20 16:54:14 -0300

    feat: clean canva

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,16 +5,17 @@ import { Header } from "../../components/header";
 const Home: React.FC = () => {
     const { img } = useContext(AuthContext)
     const divRef = useRef<HTMLDivElement>(null);
+    const cellSize = 10; // Tamanho da célula
 
     const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
         if (divRef.current) {
             const rect = divRef.current.getBoundingClientRect();
             const offsetX = event.clientX - rect.left;
             const offsetY = event.clientY - rect.top;
-            const cellSize = 10; // Tamanho da célula
             const row = Math.floor(offsetY / cellSize);
             const col = Math.floor(offsetX / cellSize);
             const square = document.createElement("div");
+            square.className = "square";
             square.style.position = "absolute";
             square.style.width = `${cellSize}px`;
             square.style.height = `${cellSize}px`;
@@ -25,13 +26,23 @@ const Home: React.FC = () => {
         }
     };
 
+    const handleLimparQuadrados = () => {
+        if (divRef.current) {
+            const squares = divRef.current.querySelectorAll(".square");
+            squares.forEach(square => square.remove()); // Remove apenas os elementos com a classe "square"
+        }
+    };
+
     return (
         <div>
             <Header />
-            <div className="w-full flex justify-center py-6">
+            <div className="w-full justify-center py-6 flex flex-row gap-10">
                 <div className="relative" ref={divRef}>
                     <img className="opacity-50 max-w-[90vw]" src={img} alt="Imagem" />
                     <div className="absolute top-0 border-white border flex flex-wrap w-full h-full" onClick={handleClick} />
+                </div>
+                <div className="flex flex-col gap-3">
+                    <button className="btn btn-primary text-white" onClick={handleLimparQuadrados}>Limpar</button>
                 </div>
             </div>
         </div>
